4827e0b0089c48c97909abf258b6e53ba4bdb30c
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2020-03-27 14:45:37 +0800

    try and use google's headers

--- helpers/headers.py
+++ helpers/headers.py
@@ -15,7 +15,17 @@ def generate_headers(num_headers):
         headers[header_text] = "ABCKJHDSAKUFGKNIUHNFKWJNEFIKJNDSKHIKIOSEUHFKSNDBKJUSBDFJKHSBDFGJKBHSDGFJUYH"
 
         headers = {
-            'cookie': 'A' * 200
+            'status': 200,
+            'content-type': r'text/html; charset=ISO-8859-1',
+            'p3p': r'CP="This is not a P3P policy! See g.co/p3phelp for more info."',
+            'server': r'gws',
+            'x-xss-protection': r'0',
+            'x-frame-options': r'SAMEORIGIN',
+            'set-cookie': r'A'*200,
+            'set-cookie': r'A'*200,
+            'alt-svc': r'quic=":443"; ma=2592000; v="46,43",h3-Q050=":443"; ma=2592000,h3-Q049=":443"; ma=2592000,h3-Q048=":443"; ma=2592000,h3-Q046=":443"; ma=2592000,h3-Q043=":443"; ma=2592000,h3-T050=":443"; ma=2592000',
+            'accept-ranges': r'none',
+            'vary': r'Accept-Encoding',
         }
 
     return headers
